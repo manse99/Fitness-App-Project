cce8bddcd87910aae9c0ece9af1ab64aa5a67572
“mansee99” <manse99@gmail.com>
2023-01-23 11:23:02 -0500

    Nav.jsx

---
+++ fitnessapp/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => (
+ <header className='navbar'>
+
+   <div className='navbar_title navbar_item'>Welcome to our Fitness Menu</div>
+  
+   <div className='login'>Login</div>
+   <div className='register'>Register</div>
+ 
+ </header>   
+  
+  );
+
+export default Nav
