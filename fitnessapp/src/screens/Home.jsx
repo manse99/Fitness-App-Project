8ff7de1525ee6cc1587a175d372f63f0cee0ffbc
Edward Olstrom <eddieolstrom1@gmail.com>
2023-01-23 11:30:29 -0500

    setting up home page

---
+++ fitnessapp/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../components/Nav.jsx'
+
+export default function Home() {
+  return (
+    <div>
+        <div>Register to Login!</div>
+        <Nav /> 
+    </div>
+
+  )
+}
