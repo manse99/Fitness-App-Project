f9eecf383749b1948005fc53faa4a9c9c7ec18b0
“mansee99” <manse99@gmail.com>
2023-01-23 15:16:10 -0500

    updated navbar link

---
+++ fitnessapp/src/Pages/About.jsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <p>About</p>
+    </div>
+  );
+};
+
+export default About;
