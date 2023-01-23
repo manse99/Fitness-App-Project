c1a373df3744558217f5c11e0edb7be7ab99d660
individual-ism <individual-ism-code@proton.me>
2023-01-23 16:06:10 -0500

    Routes work

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 mongoose.set('returnOriginal', false)
 mongoose.set('strictQuery', false);
 
-const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/workoutgods'
+const url = process.env.MONGO_URL || 'mongodb://localhost:27017/workoutgods'
 
 mongoose.connect(url).catch((err) => {
   console.log(`Error connection to mongodb: ${err.message}`)
