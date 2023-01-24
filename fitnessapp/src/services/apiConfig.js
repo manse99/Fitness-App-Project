7ff129e5a725552531e088b3842ed13fdf3a4697
Edward Olstrom <eddieolstrom1@gmail.com>
2023-01-24 11:30:46 -0500

    added services

---
+++ fitnessapp/src/services/apiConfig.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: process.env.NODE_ENV === 'production'
+        ? ''
+        : 'http://localhost:3000/api'
+})
+
+export default api;
---
+++ fitnessapp/src/services/exercises.js
@@ -0,0 +1,46 @@
+import api from "./apiConfig.js";
+
+export const getExercises = async () => {
+    try {
+        const repsonse = await api.get('/exercises')
+    } catch (error) {
+        throw error
+    }
+}
+
+export const getExercise  = async id => {
+    try {
+        const response = await api.get(`/exercises/${id}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const createExercise = async exercise => {
+    try {
+        const response = await api.post('/exercises', exercise)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const updateExercise = async (id, exercise) => {
+    try {
+        const response = await api.put(`/exercises/${id}`, exercise)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const deleteProduct = async id => {
+    try {
+        const response = await api.delete(`/exercises/${id}`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
