d27cd5b82a3ee3b4a03519cfc5fb5b348cc2d86a
Edward Olstrom <eddieolstrom1@gmail.com>
2023-01-24 13:26:57 -0500

    added deployed link into apiconfig

--- fitnessapp/src/services/apiConfig.js
+++ fitnessapp/src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-        ? ''
+        ? 'https://fitness-app-1114.herokuapp.com/'
         : 'http://localhost:3000/api'
 })
 
