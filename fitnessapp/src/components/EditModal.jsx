55a5d833288a22088bf132d907fe55dbd642c09e
Kan5661 <linkan0718@gmail.com>
2023-01-25 12:07:43 -0500

    edit exercise records

--- fitnessapp/src/components/EditModal.jsx
+++ fitnessapp/src/components/EditModal.jsx
@@ -1,41 +1,77 @@
-function EditModal() {
-    async function handleSubmit() {
+import { useState } from 'react'
+import { updateExerciseRec } from '../services/records.js';
 
+function EditModal(prop) {
+    const [editExercise, setEditExercise] = useState({
+        type: "",
+        duration: "",
+        intensity: "",
+        upperBody: true,
+        sets: "",
+        weight: ""
+    }) 
+
+    async function handleSubmit() {
+        await updateExerciseRec(prop.recId, editExercise)
     }
 
-    function handleChange() {
+    function handleChange(e) {
+        const { name, value, type } = e.target;
 
+        if (type === "number") {
+            setEditExercise((prev) => ({
+              ...prev,
+              [name]: parseInt(value),
+            }));
+          } else if (type === "radio") {
+            const str2bool = (value) => {
+              if (value === "true") return true;
+              if (value === "false") return false;
+            };
+      
+            setEditExercise((prev) => ({
+              ...prev,
+              [name]: str2bool(value),
+            }));
+          }
+          
+          else {
+            setEditExercise((prev) => ({
+              ...prev,
+              [name]: value,
+            }));
+          }
     }
 
     return(
-        <div className="editModal">
-            <div className="closeEditModal">X</div>
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder="Edit type"></input>
-                <input type='text' placeholder="Edit duration"></input>
-                <input type='text' placeholder="Edit intensity"></input>
-                <input type='text' placeholder="Edit upper/lower"></input>
-                <input type='text' placeholder="Edit weight"></input>
-                <input type='text' placeholder="Edit sets"></input>
+        <div className={prop.displayEditModal? 'editModal' : 'hideElement'}>
+            <div className="closeEditModal" onClick={() => prop.setDisplayEditModal(false)}>X</div>
+            <form onSubmit={handleSubmit}>                    
+                <input name='type' onChange={handleChange} type='text' placeholder="Edit type"></input>
+                <input name='duration' onChange={handleChange} type='text' placeholder="Edit duration"></input>
+                <input name='intensity' onChange={handleChange} type='number' placeholder="Edit intensity"></input>
+                <input name='weight' onChange={handleChange} type='number' placeholder="Edit weight"></input>
+                <input name='sets' onChange={handleChange} type='number' placeholder="Edit sets"></input>
                 <div>
-          <h3>Upper Body</h3>
-        <input
-          id="upperBody-true"
-          type="radio"
-          name="upperBody"
-          value="true"
-          onChange={handleChange}
-        />
-        <label htmlFor="upperBody-true">True</label>
-        <input
-          id="upperBody-false"
-          type="radio"
-          name="upperBody"
-          value="false"
-          onChange={handleChange}
-          />
-        <label htmlFor="upperBody-false">False</label>
-        </div>
+                    <h3>Upper Body</h3>
+                    <input
+                    id="upperBody-true"
+                    type="radio"
+                    name="upperBody"
+                    value="true"
+                    onChange={handleChange}
+                    />
+                    <label htmlFor="upperBody-true">True</label>
+                    <input
+                    id="upperBody-false"
+                    type="radio"
+                    name="upperBody"
+                    value="false"
+                    onChange={handleChange}
+                    />
+                    <label htmlFor="upperBody-false">False</label>
+                </div>
+                <input type='submit'></input>
             </form>
 
         </div>
