94313f56f03941fc8f2c2af08335b187a7be2fdb
Kan5661 <linkan0718@gmail.com>
2023-01-26 15:41:43 -0500

    ReposFit

--- fitnessapp/src/Pages/CreateExerciseRec.jsx
+++ fitnessapp/src/Pages/CreateExerciseRec.jsx
@@ -70,6 +70,7 @@ export default function CreateExerciseRec() {
   return (
     <div className="addExercisePage">
       <div className="newBackground"></div>
+      <div className="newBackground2"></div>
       <EditModal setRefreshPage={setRefreshPage} recId={recId} displayEditModal={displayEditModal} setDisplayEditModal={setDisplayEditModal} />
       <h1 className="createTitle">Add Exercise</h1>
       <form className="form" onSubmit={handleSubmit}>
@@ -135,7 +136,7 @@ export default function CreateExerciseRec() {
         </div>
         <button className="buttons" type="submit">Add</button>
       </form>
-
+      
       <div className="exerciseRecList">
         {exerciseRecs.map(exerciseRec => (
         <div className="exerciseRecs">
